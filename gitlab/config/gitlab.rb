a0090372154658518b2b64a64e99935c890ff05e
YekitKsv <nickitakrac@gmail.com>
2018-08-02 21:21:48 +0300

    upload

---
+++ gitlab/config/gitlab.rb
@@ -0,0 +1,24 @@
+gitlab_rails['ldap_enabled'] = true
+gitlab_rails['ldap_servers'] = {
+'main' => {
+  'label' => 'GitLab LDAP',
+  'host' =>  'ldap',
+  'port' => 389,
+  'uid' => 'uid',
+  'attributes' => {
+    'username' => 'uid',
+    'email' => ['mail','mailalias'],
+    'first_name' => 'givenName',
+    'last_name' => 'sn',
+    'name' => 'cn'
+  },
+  'encryption' => 'plain',
+  'allow_username_or_email_login' => 'false',
+  'verify_certificates' => true,
+  'bind_dn' => 'CN=admin,DC=nindeco,DC=com',
+  'password' => 'defender',
+  'active_directory' => true,
+  'base' => 'OU=people,DC=nindeco,DC=com',
+  'user_filter' => '(objectClass=person)'
+  }
+}
