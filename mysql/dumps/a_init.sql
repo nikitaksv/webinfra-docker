23f0c08885c003ca74fcb293f93e23228915280d
YekitKsv <nickitakrac@gmail.com>
2018-08-09 20:11:43 +0300

    nextcloud

--- mysql/dumps/a_init.sql
+++ mysql/dumps/a_init.sql
@@ -1,2 +1,6 @@
+/* Создаем схему для redmine, на случай если она не указана в .env*/
 CREATE SCHEMA IF NOT EXISTS `redmine`;
+/* Создаем схему для roundcube */
 CREATE SCHEMA IF NOT EXISTS `roundcube`;
+/* Создаем схему для nextcloud */
+CREATE SCHEMA IF NOT EXISTS `nextcloud`;
